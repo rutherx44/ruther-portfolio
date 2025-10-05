9b6e244bd4275c9b4249158f609d6fe3cc0912d7
rthrx44 <bkbrx44@gmail.com>
2025-10-05 19:41:34 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Footer from "./components/sections/Footer";
 function App() {
   return (
     <>
-      <div className="container mx-auto flex flex-col gap-12 mb-4 md:gap-[4.5rem] md:mb-6 lg:gap-[7.5rem] lg:mb-16 xl:gap-8 xl:grid xl:grid-cols-2 xl:grid-rows-[auto_auto] xl:gap-y-40 xl:px-16 transitions">
+      <div className="container mx-auto flex flex-col gap-12 mb-4 md:gap-[4.5rem] md:mb-6 lg:gap-[7.5rem] lg:mb-16 xl:gap-8 xl:grid xl:grid-cols-2 xl:grid-rows-[auto_auto] xl:gap-y-40 xl:px-16 2xl:px-0 transitions">
         <Toaster
           position="top-right"
           toastOptions={{
